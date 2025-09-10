43ad789fe95619bc9a15ab5e97a9d76512701b23
Facu <fdmoreno@comunidad.unnoba.edu.ar>
2025-09-10 19:47:41 -0300

    Login v0.0, con los archivos HTMl, CSS y JS, y un controller basico, sin conexion a base de datos. +SistemaContable.java

---
+++ src/main/java/com/proyecto/sa2/SistemaContable.java
@@ -0,0 +1,15 @@
+package com.proyecto.sa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SistemaContable {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaContable.class, args);
+	}
+
+}
